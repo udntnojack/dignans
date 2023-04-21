1af0204f802bd5df00f0bdab6218bdb83bfaa263
udntnojack <udntnojack@outlook.com>
2023-04-21 21:04:00 +0100

    finished homepage

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function hamburger(){
-    
+    var x = document.getElementById("menu");
+    if (x.style.display ==="block"){
+        x.style.display = "none"
+    }else{
+        x.style.display ="block";
+    }
 }
